fb9d4bfeb61a4905f8f0252aaa19e11171ddba61
Aryan Yadav <Aryansinghyadav56@gmail.com>
2025-01-30 10:53:01 +0530

    iheritance code is added

---
+++ repos/WiproDay4/Inheritance/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Manager : Department
+    {
+        public string mgrName { get; set; }
+
+        public override void display()
+        {
+            base.display();
+            Console.WriteLine("Manager name :"+mgrName);
+        }
+    }
+}
---
+++ repos/WiproDay4/Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using Inheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       // Console.WriteLine("Hello, World!");
+
+        Employee emp = new Employee();
+        emp.EmpId = 1;
+        emp.EmpName = "Aryan";
+        emp.display();
+
+        Department dp = new Department();
+        Console.WriteLine("Department class");
+        dp.Dept = "Developer";
+        dp.EmpId = 1;
+        dp.EmpName = "Aryan";
+        dp.Salary = 90000;
+        dp.display();
+
+        Manager mg = new Manager();
+        mg.mgrName = "John";
+        mg.EmpId = 1;
+        mg.Salary = 40000;
+        mg.Dept = "HR";
+        mg.display();
+    }
+}
