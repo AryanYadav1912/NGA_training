d1f8213421dcd947b9a4a091d6e48d26ecc16a2c
Aryan Yadav <Aryansinghyadav56@gmail.com>
2025-01-30 11:12:20 +0530

    Save local changes before merging

--- repos/WiproDay4/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ repos/WiproDay4/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9d4bfeb61a4905f8f0252aaa19e11171ddba61")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
