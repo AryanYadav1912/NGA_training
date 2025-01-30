06e488d082465fc3305628628bd19d794c2742bb
Aryan Yadav <Aryansinghyadav56@gmail.com>
2025-01-30 15:58:38 +0530

    Interface Program file added

---
+++ repos/WiproDay4/IntrfacePractice/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using IntrfacePractice;
+
+namespace INterfaceconcepts
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string ch;
+            do
+            {
+                Console.WriteLine("Enter the Shape: 1.Circle 2.Rectangle");
+                string? type = Console.ReadLine();
+
+                switch (type.ToLower())
+                {
+                    case "circle":
+                        Shape ish = new Circle() { radius = 10 };
+                        ish.CalculatedArea();
+                        break;
+                    case "rectangle":
+                        Shape rec = new rectangle() { L = 10, B = 5 };
+                        rec.CalculatedArea();
+                        break;
+                    default:
+                        Shape sq = new Circle();
+                        sq.SquareArea(5);
+                        break;
+                }
+                Console.WriteLine("Do you want to continue (yes/no):");
+                ch = Console.ReadLine();
+            } while (ch.Equals("yes"));
+
+        }
+    }
+}
---
+++ repos/WiproDay4/IntrfacePractice/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrfacePractice
+{
+    internal interface Shape
+    {
+        void CalculatedArea();
+        void SquareArea(int side)
+        {
+            Console.WriteLine("Area of Square is : " + Math.Pow(side, 2));
+
+        }
+    }
+    class Circle : Shape
+    {
+        public int radius { get; set; }
+        void Shape.CalculatedArea()
+        {
+            Console.WriteLine("Area Of Circle is : " + Math.PI * Math.Pow(radius, 2));
+
+        }
+    }
+    class rectangle : Shape
+    {
+        public int L { get; set; }
+        public int B { get; set; }
+        void Shape.CalculatedArea()
+        {
+            Console.WriteLine("Area Of Rectangle is: " + L * B);
+        }
+    }
+}
