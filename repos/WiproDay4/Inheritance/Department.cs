91d5826923d5a387e1c8f7f0fca092ee092354f2
Aryan Yadav <Aryansinghyadav56@gmail.com>
2025-01-30 10:53:01 +0530

    iheritance code is added

---
+++ repos/WiproDay4/Inheritance/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Department : Employee
+    {
+        public string Dept {  get; set; }
+        public int Salary { get; set; }
+
+        public override void display()
+        {
+            base.display();
+            Console.WriteLine(Dept + " " +  Salary);
+        }
+    }
+}
