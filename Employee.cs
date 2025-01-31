e3eae43dfa2329a496c8cd7cd1a7f8dca48c0e4c
Aryan Yadav <102041624+AryanYadav1912@users.noreply.github.com>
2025-01-31 16:55:47 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultipleInheritance;
+
+namespace MultipleInheritance
+{
+    internal interface IEmployee
+    {
+        void BasicDetails();
+        void SalaryDetails();
+
+    }
+        interface IMeeting
+    {
+
+        void AttendMeetings();
+        }
+
+
+
+    
+    class PermanentEmployee : IEmployee, IMeeting
+    {
+        public int EmpId { get; set; }
+        public string EmpName {  get; set; }
+        public int salary {  get; set; }
+        public static string OrgName = "Wipro"; // shared among all instances
+        public void BasicDetails()
+        {
+            Console.WriteLine($"Employee ID: {EmpId}, Employee Name: {EmpName} , Organisation: {OrgName}");
+            
+        }
+        public void SalaryDetails()
+        {
+            Console.WriteLine($"Salary: {salary}");
+        }
+        public void AttendMeetings()
+        {
+            Console.WriteLine("Can Attend Meetings");
+
+        }
+    }
+
+    class TemporaryEmployee : IEmployee, IMeeting
+    {
+        public int salaryPerDay { get; set; }
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public void BasicDetails()
+        {
+            Console.WriteLine($"Employee ID: {EmpId}, Employee Name: {EmpName} ");
+
+        }
+        public void SalaryDetails()
+        {
+            Console.WriteLine($"Salary: {salaryPerDay}");
+        }
+        public void AttendMeetings()
+        {
+            Console.WriteLine("Cannot Attend Meetings");
+
+        }
+
+
+
+    }
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using MultipleInheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        PermanentEmployee employee = new PermanentEmployee() { EmpId = 1, EmpName ="Aryan", salary=80000 };
+        employee.BasicDetails();
+        employee.SalaryDetails();
+        employee.AttendMeetings();
+
+        TemporaryEmployee temp = new TemporaryEmployee() { EmpId = 10, EmpName = "Abhishek", salaryPerDay = 40000 };
+        temp.BasicDetails();
+        temp.SalaryDetails();
+        temp.AttendMeetings();
+    }
+
+}
